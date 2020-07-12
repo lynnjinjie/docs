1c2133ef0853cad45cbac84c2bb6ed2bf28be4dd
jinjie <lynnjinjie@gmail.com>
2020-07-12 17:24:47 +0800

    update sh

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,22 @@ cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 echo 'docs.jjj520.net' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:lynnjinjie/docs.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://lynnjinjie:${GITHUB_TOKEN}@github.com:lynnjinjie/docs.git
+  git config --global user.name "lynnjinjie"
+  git config --global user.email "lynnjinjie@gmail.com"
+fi
 
 git init
-git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:lynnjinjie/lynnjinjie.github.io.git master
+# git push -f git@github.com:lynnjinjie/lynnjinjie.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
